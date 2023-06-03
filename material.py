ccfccf1dd4c53a2c58b0dc2e557763aa0d3d0112
NikiTricky <niki.sto2010@gmail.com>
2023-06-03 20:39:03 +0300

    Render a scene with metal fuzziness

--- material.py
+++ material.py
@@ -20,11 +20,12 @@ class Lambertian(Material):
         return scattered, attenuation, True
 
 class Metal(Material):
-    def __init__(self, color=None):
+    def __init__(self, color=None, fuzz=1):
         self.albedo = color if not color is None else Color()
+        self.fuzz = fuzz if fuzz < 1 else 1
 
     def scatter(self, r_in, rec):
         reflected = reflect(unit_vector(r_in.direction()), rec.normal)
-        scattered = Ray(rec.p, reflected)
+        scattered = Ray(rec.p, reflected + self.fuzz*random_in_unit_sphere())
         attenuation = self.albedo
         return scattered, attenuation, dot(scattered.direction(), rec.normal) > 0
